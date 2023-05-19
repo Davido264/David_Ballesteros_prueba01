861b0604fe9b299c498442f86be4acdd8c13254e
Davido264 <davidaugustoballesteros67@gmail.com>
2023-05-19 17:30:27 -0500

    David Ballesteros: 17:30

--- app/src/main/java/com/example/david_ballesteros_prueba01/ui/theme/ui/theme/MainActivity.java
+++ app/src/main/java/com/example/david_ballesteros_prueba01/ui/theme/ui/theme/MainActivity.java
@@ -1,33 +1,16 @@
 package com.example.david_ballesteros_prueba01.ui.theme.ui.theme;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.david_ballesteros_prueba01.databinding.ActivityMainBinding;
+import android.os.Bundle;
 
 import com.example.david_ballesteros_prueba01.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
     }
 }
