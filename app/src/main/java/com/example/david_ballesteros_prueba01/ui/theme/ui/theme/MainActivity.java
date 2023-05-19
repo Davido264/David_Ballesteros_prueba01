82e8b795df20aa45d6afc9969311555a495ca131
Davido264 <davidaugustoballesteros67@gmail.com>
2023-05-19 17:45:29 -0500

    David Ballesteros: 17:45

--- app/src/main/java/com/example/david_ballesteros_prueba01/ui/theme/ui/theme/MainActivity.java
+++ app/src/main/java/com/example/david_ballesteros_prueba01/ui/theme/ui/theme/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.david_ballesteros_prueba01.ui.theme.ui.theme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.material3.TopAppBarDefaults;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.david_ballesteros_prueba01.R;
 
@@ -12,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = this.findViewById(R.id.button2);
+        btn.setOnClickListener((view) -> {
+            Toast.makeText(this, " R.id.button", Toast.LENGTH_SHORT).show();
+            double divider = ( (EditText) this.findViewById(R.id.editTextNumber)).getText();
+
+
+        });
     }
+
 }
